bd87b5ddbf978757508c75df6360f696cb744024
Bartemnius <wojtekdluski@wp.pl>
2024-02-17 15:52:20 +0100

    Changes made to check answer and inform user if is correct.
    Both logic in controler and question-view were adjusted to check correct answer

--- src/main/java/com/project/quizapp/controller/QuestionController.java
+++ src/main/java/com/project/quizapp/controller/QuestionController.java
@@ -6,15 +6,18 @@ import com.project.quizapp.utils.ViewNames;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class QuestionController {
     @Autowired
     QuestionService questionService;
+    private ModelAndView questionView = new ModelAndView(ViewNames.QUESTION_VIEW);
+
 
     @GetMapping("/")
     public ModelAndView helloPage() {
@@ -23,29 +26,32 @@ public class QuestionController {
 
     @GetMapping("/randomQuestion")
     public ModelAndView randomQuestion() {
-        ModelAndView modelAndView = new ModelAndView(ViewNames.QUESTION_VIEW);
         Question question = questionService.getRandomQuestion().getBody();
-        modelAndView.addObject("question", question);
-        return modelAndView;
+        questionView.addObject("question", question);
+        return questionView;
     }
 
-    @GetMapping("/question")
-    public ModelAndView question(@RequestParam Long questionId) {
-        ModelAndView modelAndView = new ModelAndView(ViewNames.QUESTION_VIEW);
+    @GetMapping("/question/{questionId}")
+    public ModelAndView question(@PathVariable Long questionId) {
         Question question = questionService.getQuestionById(questionId).getBody();
-        modelAndView.addObject("question", question);
-        return modelAndView;
+        questionView.addObject("question", question);
+        return questionView;
     }
 
     @PostMapping("/answer")
-    public RedirectView answer(@RequestParam Long questionId, @RequestParam String ans) {
+    public String answer(@RequestParam Long questionId, @RequestParam String ans, RedirectAttributes redirectAttributes) {
         Question question = questionService.getQuestionById(questionId).getBody();
         if (question.isAnswerCorrect(ans)) {
             System.out.println("Correct!");
-            return new RedirectView("/randomQuestion");
-        }
+            redirectAttributes.addFlashAttribute("result", "success");
+            redirectAttributes.addFlashAttribute("message", "Well done! :)");
 
-        System.out.println("Not this time :(");
-        return new RedirectView("/question?questionId=" + questionId);
+        } else {
+            System.out.println("Not this time :(");
+            redirectAttributes.addFlashAttribute("result", "failure");
+            redirectAttributes.addFlashAttribute("message", "Not this time! :(");
+
+        }
+        return "redirect:/question/" + questionId;
     }
 }
