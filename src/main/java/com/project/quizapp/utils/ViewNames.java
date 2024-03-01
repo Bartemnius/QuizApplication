c5e54ec4cc8e9f79cf964443b96d31df8770d256
Bartemnius <wojtekdluski@wp.pl>
2024-03-01 10:25:45 +0100

    Add new view for questions by category.
    Minor changes for other views.

--- src/main/java/com/project/quizapp/utils/ViewNames.java
+++ src/main/java/com/project/quizapp/utils/ViewNames.java
@@ -2,5 +2,7 @@ package com.project.quizapp.utils;
 
 public class ViewNames {
     public static final String MAIN_VIEW =  "main-view";
-    public static final String QUESTION_VIEW = "question-view";
+    public static final String QUESTION_VIEW = "random-question-view";
+
+    public static final String QUESTIONS_BY_CATEGORY = "questions-category-view";
 }
